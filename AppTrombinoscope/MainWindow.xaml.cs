8d14932afb37c837c3185bdf61f61ff7d4265d7b
lenisrl <lenisorel.77@gmail.com>
2024-03-06 11:55:28 +0100

    add: menubar for the main page

--- AppTrombinoscope/MainWindow.xaml.cs
+++ AppTrombinoscope/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace AppTrombinoscope
         {
             InitializeComponent();
         }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
