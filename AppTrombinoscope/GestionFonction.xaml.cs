a26517d22870de16151d6dbf70049d87c9b77da0
leni <sorel223@etu.uniaen.fr>
2024-03-07 19:04:28 +0100

    add: vue personnel fix: nommages de la page fonctions

--- AppTrombinoscope/GestionFonction.xaml.cs
+++ AppTrombinoscope/GestionFonction.xaml.cs
@@ -1,18 +1,6 @@
 ﻿using BddpersonnelContext;
 using DllbddPersonnels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace AppTrombinoscope
 {
@@ -50,7 +38,7 @@ namespace AppTrombinoscope
 
         }
 
-        private void buttonAddService_Click(object sender, RoutedEventArgs e)
+        private void ButtonAddFct_Click(object sender, RoutedEventArgs e)
         {
             try
             {
