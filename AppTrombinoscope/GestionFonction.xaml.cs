3d2492158be704924d960263d264a4461e898bdd
leni <sorel223@etu.uniaen.fr>
2024-03-07 15:55:39 +0100

    add: fonction gestion windows

--- AppTrombinoscope/GestionFonction.xaml.cs
+++ AppTrombinoscope/GestionFonction.xaml.cs
@@ -1,18 +1,27 @@
-﻿using System.Windows;
-using DllbddPersonnels;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
 
 namespace AppTrombinoscope
 {
-    public partial class GestionService : Window
+    /// <summary>
+    /// Logique d'interaction pour GestionFonction.xaml
+    /// </summary>
+    public partial class GestionFonction : Window
     {
-        bddpersonnels bddPersonnels;
-
-        public GestionService(bddpersonnels bddPersonnels)
+        public GestionFonction()
         {
-            this.bddPersonnels = bddPersonnels;
             InitializeComponent();
         }
-
-
     }
 }
